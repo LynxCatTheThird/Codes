31f0b4485b9e2bd5451dc046a1650380a7ec1768
LynxCatTheThird <tsfxlyx@qq.com>
2024-07-27 21:13:32 +0800

    feat: 自动判断依赖是否存在

--- C++/Hexo/t.cpp
+++ C++/Hexo/t.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
 #include <cstdlib>
+#include <fstream>
 using namespace std;
+
+bool isDependenciesPresent(const string& filename, const string& dependencie) {
+    ifstream file(filename); // 打开文件
+    if (!file.is_open()) {
+        cerr << "无法打开"+filename<< endl;
+        return false;
+    }
+
+    string line;
+    while (getline(file, line)) { // 逐行读取文件
+        if (line.find(dependencie) != string::npos) { // 查找是否含有预期字符串
+            return true;
+        }
+    }
+
+    return false;
+}
 
 void hexoClean(){
   cout<<"Cleaning...\n";
   system("hexo clean > /dev/null");
-  cout<<"Cleaned.\n";
 }
 
 void hexoServer(){
@@ -19,7 +36,16 @@ void hexoBuild(){
   cout<<"Generating...\n";
   system("hexo generate > /dev/null");
   cout<<"Optimizing...\n";
-  system("hexo swpp&&gulp zip");
+  if (isDependenciesPresent("package.json", "swpp")){
+    system("hexo swpp");
+  }else{
+    cout << "本地项目中不包含 swpp 或无法判断状态" << endl;
+  }
+  if (isDependenciesPresent("package.json", "gulp")){
+    system("gulp zip");
+  }else{
+    cout << "本地项目中不包含 gulp 或无法判断状态" << endl;
+  }
   cout<<"Deploying...\n";
   system("hexo d");
   hexoClean();
