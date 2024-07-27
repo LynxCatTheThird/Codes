bc799ee1ef5e37fd7f4a844a20252bfa63580600
LynxCatTheThird <tsfxlyx@qq.com>
2024-07-27 21:12:49 +0800

    refactor: 模块化 fix: 修复部分错误命令

--- C++/Hexo/t.cpp
+++ C++/Hexo/t.cpp
@@ -2,24 +2,42 @@
 #include <cstdlib>
 using namespace std;
 
+void hexoClean(){
+  cout<<"Cleaning...\n";
+  system("hexo clean > /dev/null");
+  cout<<"Cleaned.\n";
+}
+
+void hexoServer(){
+  hexoClean();
+  cout<<"Starting...\n";
+  system("hexo server > /dev/null");
+}
+
+void hexoBuild(){
+  hexoClean();
+  cout<<"Generating...\n";
+  system("hexo generate > /dev/null");
+  cout<<"Optimizing...\n";
+  system("hexo swpp&&gulp zip");
+  cout<<"Deploying...\n";
+  system("hexo d");
+  hexoClean();
+}
+
 int main(int argc, char* argv[]) {
-  if (argc == 1 || string(argv[1]) == "h") { // 如果没有参数或参数为h
-    cout << "帮助信息：\n";
-    cout << "t b: 执行hexo cl && hexo g && hexo d && hexo cl\n";
-    cout << "t ut: 更新主题\n";
-    cout << "t u: 执行ncu -u && npm install\n";
-    cout << "t s: 执行hexo cl && hexo g && hexo se\n";
-    cout << "t h: 输出帮助信息\n";
-  } else if (string(argv[1]) == "b") { // 如果参数为b
-    system("hexo cl && hexo g && hexo d && hexo cl"); // 执行系统命令
-  } else if (string(argv[1]) == "ut") { // 如果参数为ut
-    system("git submodule update --remote --merge"); // 执行系统命令
-  } else if (string(argv[1]) == "u") { // 如果参数为u
-    system("ncu -u && npm install"); // 执行系统命令
-  } else if (string(argv[1]) == "s" || string(argv[1]) == "se") { // 如果参数为s
-    system("hexo cl && hexo g && hexo se"); // 执行系统命令
+  if (argc == 1) {
+    cout << "无效的参数\n";
+  } else if (string(argv[1]) == "b") {
+    hexoBuild();
+  } else if (string(argv[1]) == "ut") {
+    system("git submodule update --remote --merge");
+  } else if (string(argv[1]) == "u") {
+    system("ncu -u && npm install");
+  } else if (string(argv[1]) == "s" || string(argv[1]) == "se") {
+    hexoServer();
   } else {
-    cout << "无效的参数\n"; // 其他情况输出错误信息
+    cout << "无效的参数\n";
   }
   return 0;
 }
