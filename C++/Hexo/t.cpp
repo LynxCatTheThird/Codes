0e2483e1e916f4a34d509b4fde3acdc06acb0a47
LynxCatTheThird <tsfxlyx@qq.com>
2024-07-27 21:14:20 +0800

    refactor: 标准化 C++ 行为

--- C++/Hexo/t.cpp
+++ C++/Hexo/t.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <cstdlib>
 #include <fstream>
-using namespace std;
+#include <string>
 
-bool isDependenciesPresent(const string& filename, const string& dependencie) {
-    ifstream file(filename); // 打开文件
+bool isDependenciesPresent(const std::string& filename, const std::string& dependencie) {
+    std::ifstream file(filename); // 打开文件
     if (!file.is_open()) {
-        cerr << "无法打开"+filename<< endl;
+        std::cerr << "无法打开" + filename << std::endl;
         return false;
     }
 
-    string line;
-    while (getline(file, line)) { // 逐行读取文件
-        if (line.find(dependencie) != string::npos) { // 查找是否含有预期字符串
+    std::string line;
+    while (std::getline(file, line)) { // 逐行读取文件
+        if (line.find(dependencie) != std::string::npos) { // 查找是否含有预期字符串
             return true;
         }
     }
@@ -20,50 +20,50 @@ bool isDependenciesPresent(const string& filename, const string& dependencie) {
     return false;
 }
 
-void hexoClean(){
-  cout<<"Cleaning...\n";
-  system("hexo clean > /dev/null");
+void hexoClean() {
+    std::cout << "Cleaning...\n";
+    std::system("hexo clean > /dev/null");
 }
 
-void hexoServer(){
-  hexoClean();
-  cout<<"Starting...\n";
-  system("hexo server > /dev/null");
+void hexoServer() {
+    hexoClean();
+    std::cout << "Starting...\n";
+    std::system("hexo server > /dev/null");
 }
 
-void hexoBuild(){
-  hexoClean();
-  cout<<"Generating...\n";
-  system("hexo generate > /dev/null");
-  cout<<"Optimizing...\n";
-  if (isDependenciesPresent("package.json", "swpp")){
-    system("hexo swpp");
-  }else{
-    cout << "本地项目中不包含 swpp 或无法判断状态" << endl;
-  }
-  if (isDependenciesPresent("package.json", "gulp")){
-    system("gulp zip");
-  }else{
-    cout << "本地项目中不包含 gulp 或无法判断状态" << endl;
-  }
-  cout<<"Deploying...\n";
-  system("hexo d");
-  hexoClean();
+void hexoBuild() {
+    hexoClean();
+    std::cout << "Generating...\n";
+    std::system("hexo generate > /dev/null");
+    std::cout << "Optimizing...\n";
+    if (isDependenciesPresent("package.json", "swpp")) {
+        std::system("hexo swpp");
+    } else {
+        std::cout << "本地项目中不包含 swpp 或无法判断状态" << std::endl;
+    }
+    if (isDependenciesPresent("package.json", "gulp")) {
+        std::system("gulp zip");
+    } else {
+        std::cout << "本地项目中不包含 gulp 或无法判断状态" << std::endl;
+    }
+    std::cout << "Deploying...\n";
+    std::system("hexo d");
+    hexoClean();
 }
 
 int main(int argc, char* argv[]) {
-  if (argc == 1) {
-    cout << "无效的参数\n";
-  } else if (string(argv[1]) == "b") {
-    hexoBuild();
-  } else if (string(argv[1]) == "ut") {
-    system("git submodule update --remote --merge");
-  } else if (string(argv[1]) == "u") {
-    system("ncu -u && npm install");
-  } else if (string(argv[1]) == "s" || string(argv[1]) == "se") {
-    hexoServer();
-  } else {
-    cout << "无效的参数\n";
-  }
-  return 0;
+    if (argc == 1) {
+        std::cout << "无效的参数\n";
+    } else if (std::string(argv[1]) == "b") {
+        hexoBuild();
+    } else if (std::string(argv[1]) == "ut") {
+        std::system("git submodule update --remote --merge");
+    } else if (std::string(argv[1]) == "u") {
+        std::system("ncu -u && npm install");
+    } else if (std::string(argv[1]) == "s" || std::string(argv[1]) == "se") {
+        hexoServer();
+    } else {
+        std::cout << "无效的参数\n";
+    }
+    return 0;
 }
