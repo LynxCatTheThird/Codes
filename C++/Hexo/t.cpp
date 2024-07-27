66da2ee15088702db35d9766a4658b9e9c4a6dc1
LynxCatTheThird <tsfxlyx@qq.com>
2024-07-27 21:09:59 +0800

    fix: 为 hexo server 附加 swpp

--- C++/Hexo/t.cpp
+++ C++/Hexo/t.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main(int argc, char* argv[]) {
   if (argc == 1 || string(argv[1]) == "h") { // 如果没有参数或参数为h
     cout << "帮助信息：\n";
-    cout << "t b: 执行hexo c && hexo g && hexo swpp && hexo d && hexo c\n";
-    cout << "t ut: 更新主题butterfly\n";
+    cout << "t b: 执行hexo cl && hexo g && hexo swpp && hexo d && hexo cl\n";
+    cout << "t ut: 更新主题\n";
     cout << "t u: 执行ncu -u && npm install\n";
     cout << "t s: 执行hexo cl && hexo g && hexo swpp && hexo se\n";
     cout << "t h: 输出帮助信息\n";
@@ -17,7 +17,7 @@ int main(int argc, char* argv[]) {
   } else if (string(argv[1]) == "u") { // 如果参数为u
     system("ncu -u && npm install"); // 执行系统命令
   } else if (string(argv[1]) == "s") { // 如果参数为s
-    system("hexo cl && hexo se"); // 执行系统命令
+    system("hexo cl && hexo g && hexo swpp && hexo se"); // 执行系统命令
   } else {
     cout << "无效的参数\n"; // 其他情况输出错误信息
   }
